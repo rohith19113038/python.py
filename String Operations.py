72cbf8d1d51fd17f9a2a11031ac22af5137a39c1
Rohith Ganesh <72005701+rohith19113038@users.noreply.github.com>
2020-10-10 10:31:55 +0530

    Create String Operations.py

---
+++ String Operations.py
@@ -0,0 +1,32 @@
+a = "Hello, World!"
+print(a[1])
+
+b = "how you doing"
+print(b[2:5])
+
+b = "pizza boy"
+print(b[-5:-2])
+
+a =  "ALIVE"
+print(len(a))
+
+a = "YOU HAVE FAILED THIS CITY"
+print(a.lower())
+
+a = "vampire diaries"
+print(a.upper())
+
+a = "originals"
+print(a.replace("g", "J"))
+
+a = "Hi"
+b = "Bro"
+c = a + " " + b
+print(c)
+
+age = 18
+txt = "My name is rohith, I am " + str(age)
+print(txt)
+
+A="FLASH"
+print(A*3)
