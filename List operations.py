fcdb7cb5c7788a83c8050c24dd9347b9854582af
Rohith Ganesh <72005701+rohith19113038@users.noreply.github.com>
2020-10-10 10:32:50 +0530

    Create List operations.py

---
+++ List operations.py
@@ -0,0 +1,36 @@
+my_list = ["apple", "banana", "cherry"]
+print(my_list)
+print(my_list[1])
+print(my_list[-1])
+my_list = ["hello","hi","bye-bye","forever","earth","yellow"]
+print(my_list[2:5])
+print(my_list[1:-1])
+my_list[1] = "blue"
+print(my_list)
+my_list = ["apple", "banana", "cherry"]
+for x in my_list:
+  print(x)
+my_list = ["apple", "banana", "cherry"]
+print(len(my_list))  
+my_list = ["apple", "banana", "cherry"]
+my_list.append("orange")
+print(my_list)
+my_list = ["apple", "banana", "cherry"]
+my_list.insert(1, "orange")
+print(my_list)
+my_list = ["apple", "banana", "cherry"]
+my_list.remove("banana")
+print(my_list)
+my_list = ["apple", "banana", "cherry"]
+my_list.pop()
+print(my_list)
+my_list = ["apple", "banana", "cherry"]
+del my_list
+my_list = ["apple", "banana", "cherry"]
+mylist = my_list.copy()
+print(mylist)
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+list1.extend(list2)
+print(list1)
